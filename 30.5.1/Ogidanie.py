82008844630a06a640ea29aceddcfa90f026c638
Skifchik666 <Skifchik666>
2023-12-03 18:26:29 +0900

    30.5.1

--- 30.5.1/Ogidanie.py
+++ 30.5.1/Ogidanie.py
@@ -29,13 +29,13 @@ def testing():
 
 def test_show_all_pets():
 
-   # Открываем страницу мои питомцы.
+   # Открываем страницу все питомцы.
    pytest.driver.find_element(By.CSS_SELECTOR, 'a[href="/"]').click()
 
    # Настраиваем неявные ожидания:
    pytest.driver.implicitly_wait(10)
 
-   # Проверяем, что мы оказались на главной странице
+   # Проверяем, что мы оказались на главной странице (все питомцы)
    assert pytest.driver.find_element(By.TAG_NAME, 'h1').text == "PetFriends"
 
    # Ищем на странице все фотографии, имена, породу и возраст питомцев:
@@ -43,7 +43,7 @@ def test_show_all_pets():
    names = pytest.driver.find_elements(By.CSS_SELECTOR, '.card-deck .card-title')
    descriptions = pytest.driver.find_elements(By.CSS_SELECTOR, '.card-deck .card-text')
 
-   # Проверяем, что на странице есть фотографии питомцев, имена, порода и возраст питомцев не пустые строки:
+   # Проверяем, что на странице есть фотографии питомцев, имена, порода и возраст питомцев не пустые:
    for i in range(len(names)):
       assert images[i].get_attribute('src') != ''
       assert names[i].text != ''
@@ -63,7 +63,7 @@ def test_show_my_pets():
    # Проверяем, что мы оказались на  странице пользователя, ожидая, что в течение 10с на странице есть тег h2 с тименем пользователя "Vasya"
    assert wait.until(EC.text_to_be_present_in_element((By.TAG_NAME, 'h2'), 'Vasya'))
 
-   # Ищем в теле таблицы все строки с полными данными питомцев (имя, порода, возраст):
+   # Ищем в таблице все строки с полными данными питомцев (имя, порода, возраст):
    css_locator = 'tbody>tr'
    data_my_pets = pytest.driver.find_elements(By.CSS_SELECTOR, css_locator)
 
@@ -71,7 +71,7 @@ def test_show_my_pets():
    for i in range(len(data_my_pets)):
       assert wait.until(EC.visibility_of(data_my_pets[i]))
 
-   # Ищем в таблицt все фотографии питомцев, ожидаея, что все загруженные фото, видны на странице:
+   # Ищем в таблице все фотографии питомцев, ожидаея, что все загруженные фото, видны на странице:
    image_my_pets = pytest.driver.find_elements(By.CSS_SELECTOR, 'img[style="max-width: 100px; max-height: 100px;"]')
    for i in range(len(image_my_pets)):
       if image_my_pets[i].get_attribute('src') != '':
